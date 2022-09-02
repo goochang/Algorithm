20157c83b03ed4953abaa73e36e42320380fa640
ChoiChangkyu <cck742@naver.com>
2022-09-02 11:18:16 +0900

    Update code.js

--- Programmers/step1/code.js
+++ Programmers/step1/code.js
@@ -1,15 +1,18 @@
 function solution(arr, divisor) {
     var answer = [];
     
+    // 나누어 떨어지는 값만 배열에 저장
     for(var i=0;i<=arr.length;i++){
         if(arr[i] % divisor == 0){
             answer.push(arr[i]);
         }
     }
     
-    if(answer.length == 0){
+    // 저장된 값이 없는 경우 -1만 리턴
+    if(answer.length == 0){ 
         answer.push(-1);
     } else {
+        // 오름차순 정렬
         answer.sort(function(a, b) {
           return a - b;
         });
